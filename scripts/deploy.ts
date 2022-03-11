db643f2c4980dd49125bfe6a63252968ef4bbac5
yeosiowvic <victoryeo1974@gmail.com>
2022-03-11 14:41:41 +0800

    update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,10 +1,15 @@
 import { ethers } from 'hardhat';
 
 async function main() {
-	const testFactory = await ethers.getContractFactory('BondMaker');
-	const testInstance = await testFactory.deploy();
-	console.log('Contract deployed to address:', testInstance.address);
-  console.log((testInstance.deployTransaction.gasLimit))
+	const testFactory = await ethers.getContractFactory('BondTokenFactory');
+	const testFactoryInstance = await testFactory.deploy();
+	console.log('Factory Contract deployed to address:', testFactoryInstance.address);
+  console.log((testFactoryInstance.deployTransaction.gasLimit))
+
+  const testMaker = await ethers.getContractFactory('BondMaker');
+	const testMakerInstance = await testMaker.deploy(18, testFactoryInstance.address);
+	console.log('Maker Contract deployed to address:', testMakerInstance.address);
+  console.log((testMakerInstance.deployTransaction.gasLimit))
 }
 
 main()
